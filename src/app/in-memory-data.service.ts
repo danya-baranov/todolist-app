82eac36184df0c87bd6713aa6c5bbf33da9dcb39
danya-baranov <danyabaranov1997@gmail.com>
2019-09-17 17:27:33 +0300

    add

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class InMemoryDataService implements InMemoryDbService{
-  createDb(){
+  createDb() {
     const items = [
       {
         id: 1,
